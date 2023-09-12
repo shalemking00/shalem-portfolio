bc2744d5143706022dd3eca742a73457e121c52a
Kaneti <2076212@cognizant.com>
2023-09-12 10:54:32 +0530

    modified package.json

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -4,7 +4,7 @@ import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
-import Testimonial from "../Testimonials";
+
 
 export default function Home() {
   return (
